c55bbfa30d53d7a7169dfd5a6211a85a37356611
Avery Howard <avery.d.howard@gmail.com>
2025-02-14 07:45:47 -0500

    Basic login acheived

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  modules: [
+    'nuxt-vue3-google-signin'
+  ],
+  googleSignIn: {
+    clientId: '573857706739-1b2du7fb2grdveig2pigjh3abhdeg6op.apps.googleusercontent.com',
+  }
 })
